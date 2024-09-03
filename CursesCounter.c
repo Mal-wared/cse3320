988debdf4b13fc305e537d3846ffd1aba3d7ea07
Nicholas <nvtran504@gmail.com>
2024-09-03 18:47:18 -0500

    Curses experimentation

--- CursesCounter.c
+++ CursesCounter.c
@@ -11,13 +11,38 @@
 int main()
 {
   int i;
-  initscr(); /* Start curses mode              */
-  for (i = 0; i < 1000; i++)
+  char command[16];
+  initscr(); /* Start curses mode */
+  for (i = 0; i < 1000; i+=5)
   {
+    // Keep track of entered character
+    move(0, 0);
+    printw("Previous command: ");
+
+    // Keep track of entered character
+    move(1, 0);
+    printw("Enter a command: ");
+
+    // Print the current number and the number + 5
     move(4, 8);
-    printw("%d", i); /* Print Hello World              */
-    refresh();       /* Print it on to the real screen */
-    sleep(1);
+    printw("%d %d", i,i+5);
+
+    // Print onto screen
+    refresh();
+
+    // Get user input
+    move(1, 17);
+    getnstr(command, 16);
+    refresh();
+
+    // Erase user's input for cleanliness and replace previous input for tracking
+    move(1, 0);
+    clrtoeol();
+    printw("Enter a command: ");
+    move(0, 0);
+    clrtoeol();
+    printw("Previous command: %s", command);
+    refresh();
   }
   getch();  /* Wait for user input */
   endwin(); /* End curses mode                */
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # cse3320
 
-sudo apt-get install libncurses-dev
-sudo apt-get install ncurses-dev
-sudo apt-get install libncurses5-dev libncursesw5-dev
-sudo apt-get install libncurses5-dev libncursesw5-dev
+# Curses Installation Instructions
+1. sudo apt-get update
+2. sudo apt-get install libncurses5-dev libncursesw5-dev
