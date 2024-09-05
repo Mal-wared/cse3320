cf7823ca68f28aa54cf31f2e9bc1487fcaf08718
Nicholas <nvtran504@gmail.com>
2024-09-05 16:35:56 -0500

    added switch case for commands

--- CursesCounter.c
+++ CursesCounter.c
@@ -64,7 +64,6 @@ int main()
   
   while ((directory_entry = readdir(curr_directory)))
   {
-    
     if (((directory_entry->d_type) & DT_DIR) && count != 0) {
       printw("%d. %s", count++, directory_entry->d_name);
       file_place++;
@@ -77,20 +76,122 @@ int main()
       move(file_place, 15);
       count++;
     }
-    
-      
   }
-  
+
   closedir(curr_directory);
-   
+
+  // Display available operations
+  file_place ++;
+  move(file_place, 0);  
+  printw("Operations: ");
+  move(file_place++, 15);
+  printw("D Display");
+  move(file_place++, 15);
+  printw("E Edit");
+  move(file_place++, 15);
+  printw("R Run");
+  move(file_place++, 15);
+  printw("S Sort Directory listing");
+  
+  
   while(strcmp(command, "Q"))
   {
     // Get user input
-    move(4, 17);
+    move(++file_place, 15);
     getnstr(command, 16);
+    switch(command[0]){
+      case 'D': {
+        clear();
+
+        // Print working directory
+        move(0, 0);
+        getcwd(current_directory, 200);
+        printw("Current Working Dir: %s", current_directory);
+
+        // Get current time
+        move(1, 0);
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        printw("It is now: %s", asctime(timeinfo));
+
+        // Get Files
+        count = 0;
+        file_place = 3;
+        move(file_place, 0);
+        curr_directory = opendir(".");
+        count = 0;
+
+        while ((directory_entry = readdir(curr_directory)) && count != 5)
+        {
+          if (((directory_entry->d_type) & DT_REG) && count != 0) {
+            printw("%d. %s", count++, directory_entry->d_name);
+            file_place++;
+            move(file_place, 15);
+          } else if (((directory_entry->d_type) & DT_REG)) {
+            printw("Files:");
+            move(file_place, 15);
+            printw("%d. %s", count++, directory_entry->d_name);
+            file_place++; 
+            move(file_place, 15);
+          }
+            
+          if (count == 5)
+          {
+            file_place++;
+            move(file_place, 0);
+          }
+        }
+        rewinddir(curr_directory);
+        
+        // Get directories
+        count = 0;
+        
+        while ((directory_entry = readdir(curr_directory)))
+        {
+          if (((directory_entry->d_type) & DT_DIR) && count != 0) {
+            printw("%d. %s", count++, directory_entry->d_name);
+            file_place++;
+            move(file_place, 15);
+          } else if ((directory_entry->d_type) & DT_DIR) {
+            printw("Directories: ");
+            move(file_place, 15);
+            printw("%d. %s", count++, directory_entry->d_name);
+            file_place++;
+            move(file_place, 15);
+            count++;
+          }
+        }
+
+        closedir(curr_directory);
+
+        // Display available operations
+        file_place ++;
+        move(file_place, 0);  
+        printw("Operations: ");
+        move(file_place++, 15);
+        printw("D Display");
+        move(file_place++, 15);
+        printw("E Edit");
+        move(file_place++, 15);
+        printw("R Run");
+        move(file_place++, 15);
+        printw("S Sort Directory listing");
+        break;
+      }
+      case 'E': {
+        break;
+      }
+      case 'R': {
+        break;
+      }
+      case 'S': {
+        break;
+      }
+    }
+
     refresh();
   }
-  endwin(); /* End curses mode                */
+  endwin(); /* End curses mode */
 
   return 0;
 }
